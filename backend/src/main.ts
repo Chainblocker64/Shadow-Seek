bdb8cc5ea0a1d7cbeef0e5fa7c78017528365de4
Marcel <marcel.grieb+neuefische@gmail.com>
2026-07-16 15:06:16 +0200

    enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigin = process.env.ALLOWED_ORIGIN || '';
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -16,6 +17,14 @@ async function bootstrap() {
       },
     }),
   );
+
+  app.enableCors({
+    origin: allowedOrigin,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    // todo: when (if!) switching to cookies, uncomment this line:
+    //credentials: true,
+    allowedHeaders: 'Content-Type, Authorization',
+  });
 
   await app.listen(process.env.PORT ?? 3001);
 }
