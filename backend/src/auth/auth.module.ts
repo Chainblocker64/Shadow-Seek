84ece88a97d4ba1cc113009cf6d9905b9cf7b6af
Marcel <marcel.grieb+neuefische@gmail.com>
2026-07-16 11:36:31 +0200

    cleanup

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,6 +1,5 @@
-// src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config'; // Import these
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
