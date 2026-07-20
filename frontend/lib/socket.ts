79d454bf777f65d6b863dbcd74290c881217f0a7
Keno <keno.mandrysch@gmail.com>
2026-07-20 18:22:59 +0200

    Unused variable for CI lint test

--- frontend/lib/socket.ts
+++ frontend/lib/socket.ts
@@ -2,6 +2,8 @@ import { io, Socket } from "socket.io-client";
 
 const SOCKET_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
+const test = 123;
+
 export const socket: Socket = io(SOCKET_URL, {
-  autoConnect: falsse,
+  autoConnect: false,
 });
