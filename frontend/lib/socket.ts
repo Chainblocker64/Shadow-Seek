596dc6ec06bd10b1e8114c83e057260ddfad5fbd
Keno <keno.mandrysch@gmail.com>
2026-07-20 18:27:35 +0200

    Test for lint errors

--- frontend/lib/socket.ts
+++ frontend/lib/socket.ts
@@ -2,7 +2,12 @@ import { io, Socket } from "socket.io-client";
 
 const SOCKET_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
-const test = 123;
+const test = 15;
+const test = {
+  name: "hey",
+  email: "@",
+  name: "hi",
+};
 
 export const socket: Socket = io(SOCKET_URL, {
   autoConnect: false,
