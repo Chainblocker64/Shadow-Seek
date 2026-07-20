76e1285e96b0d10677354bdde6aac1806a7f57df
Keno <keno.mandrysch@gmail.com>
2026-07-20 18:16:32 +0200

    Syntax error test for CI detection

--- frontend/lib/socket.ts
+++ frontend/lib/socket.ts
@@ -3,5 +3,5 @@ import { io, Socket } from "socket.io-client";
 const SOCKET_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export const socket: Socket = io(SOCKET_URL, {
-  autoConnect: false,
+  autoConnect: falsse,
 });
