d3dc5c61e6bdf5465fc51d3e9910ea085958a339
Keno <keno.mandrysch@gmail.com>
2026-07-20 18:30:01 +0200

    Revert test changes

--- frontend/lib/socket.ts
+++ frontend/lib/socket.ts
@@ -2,13 +2,6 @@ import { io, Socket } from "socket.io-client";
 
 const SOCKET_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
-const test = 15;
-const test = {
-  name: "hey",
-  email: "@",
-  name: "hi",
-};
-
 export const socket: Socket = io(SOCKET_URL, {
   autoConnect: false,
 });
