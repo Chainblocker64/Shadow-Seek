47eec70b11e7cdda58532c89167a81f375c7f3d0
Keno <keno.mandrysch@gmail.com>
2026-07-21 18:06:50 +0200

    Move room state into page to enable view switch while keeping the socket

--- frontend/app/lobby/components/RoomList.tsx
+++ frontend/app/lobby/components/RoomList.tsx
@@ -1,31 +1,13 @@
-"use client";
-import { useEffect, useState } from "react";
 import { Room } from "../types";
-import { socket } from "@/lib/socket";
 import RoomListItems from "./RoomListItems";
 
-export default function RoomList() {
-  const [rooms, setRooms] = useState<Room[]>([]);
-
-  useEffect(() => {
-    socket.connect();
-
-    const onRoomsSync = (rooms: Room[]) => {
-      setRooms(rooms);
-    };
-
-    socket.on("rooms:sync", onRoomsSync);
-
-    return () => {
-      socket.off("rooms:sync", onRoomsSync);
-      socket.disconnect();
-    };
-  }, []);
-
-  const handleCreateRoom = () => {
-    socket.emit("createRoom");
-  };
-
+export default function RoomList({
+  rooms,
+  handleCreateRoom,
+}: {
+  rooms: Room[];
+  handleCreateRoom: () => void;
+}) {
   return (
     <div className="relative px-4 sm:px-6 lg:px-8 py-8 outline outline-white/30 rounded-3xl flex flex-1 flex-col">
       <div className="mb-6 flex items-center justify-between lg:justify-center">
--- frontend/app/lobby/page.tsx
+++ frontend/app/lobby/page.tsx
@@ -1,11 +1,37 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import RoomList from "./components/RoomList";
+import { socket } from "@/lib/socket";
+import { Room } from "./types";
 
 export default function Lobby() {
+  const [rooms, setRooms] = useState<Room[]>([]);
+
+  useEffect(() => {
+    socket.connect();
+
+    const onRoomsSync = (rooms: Room[]) => {
+      setRooms(rooms);
+    };
+
+    socket.on("rooms:sync", onRoomsSync);
+
+    return () => {
+      socket.off("rooms:sync", onRoomsSync);
+      socket.disconnect();
+    };
+  }, []);
+
+  const handleCreateRoom = () => {
+    socket.emit("createRoom");
+  };
+
   return (
     <main className="flex w-full flex-1 flex-col items-center px-6 py-10">
       <div className="flex w-full max-w-6xl flex-1 flex-col px-6 py-8 sm:px-10">
         <div className="mx-auto flex w-full max-w-2xl flex-1 flex-col">
-          <RoomList />
+          <RoomList handleCreateRoom={handleCreateRoom} rooms={rooms} />
         </div>
       </div>
     </main>
