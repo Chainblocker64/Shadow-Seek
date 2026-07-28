860ca89f313bc20cf87e6bbb02880ea2a608e2c4
Sergey Badin <sergeykfg1@me.com>
2026-07-28 08:42:45 +0200

    feat: add player movement to game service

--- backend/src/game/game.service.ts
+++ backend/src/game/game.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { RUNNING, WAITING } from './consts';
-import type { GameState, Position, GameMap } from './types';
+import type {
+  GameMap,
+  GameState,
+  MovementDirection,
+  MovementResult,
+  Position,
+} from './types';
 import type { ClientId, RoomId } from '../shared/types';
+import { handlePlayerMovement } from './movement/server-movement';
 
 @Injectable()
 export class GameService {
@@ -33,6 +40,20 @@ export class GameService {
 
   getGame(roomId: RoomId): GameState | undefined {
     return this.games.get(roomId);
+  }
+
+  movePlayer(
+    roomId: RoomId,
+    playerId: ClientId,
+    direction: MovementDirection,
+  ): MovementResult | undefined {
+    const game = this.games.get(roomId);
+
+    if (!game || game.status !== RUNNING) {
+      return;
+    }
+
+    return handlePlayerMovement(game, playerId, direction);
   }
 
   startGame(roomId: RoomId): GameState | undefined {
